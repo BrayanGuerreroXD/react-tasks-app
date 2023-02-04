09ac3f2102168776dd6668d5c8f796bad1ddbe7a
BrayanGuerreroXD <bg7047653@gmail.com>
2023-02-04 16:27:33 -0500

    Create context component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import TaskList from "./components/TaskLists";
 import TaskForm from "./components/TaskForm";
 import { tasks as data } from "./data/tasks";
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from "react";
 
 function App() {
   const [tasks, setTasks] = useState([]);
@@ -10,22 +10,10 @@ function App() {
     setTasks(data);
   }, []);
 
-  function createTask(task) {
-    setTasks([...tasks, {
-      id: tasks.length,
-      title: task.title,
-      description: task.description
-    }])
-  }
-
-  function deleteTasks(taskId) {
-    setTasks(tasks.filter(task => task.id != taskId))
-  }
-
   return (
     <div>
-      <TaskForm createTask = {createTask}/>
-      <TaskList tasks = {tasks} deleteTasks = {deleteTasks}/>
+      <TaskForm createTask={createTask} />
+      <TaskList tasks={tasks} deleteTasks={deleteTasks} />
     </div>
   );
 }
