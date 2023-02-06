303957c72b471df86dde63850732be897c707fb2
BrayanGuerreroXD <bg7047653@gmail.com>
2023-02-05 23:29:06 -0500

    Add styles to app with tailwindcss

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -17,20 +17,25 @@ function TaskForm() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        placeholder="Add task"
-        onChange={(e) => setTitle(e.target.value)}
-        value={title}
-        autoFocus
-      />
-      <textarea
-        placeholder="Task description"
-        onChange={(e) => setDescription(e.target.value)}
-        value={description}
-      />
-      <button>Save</button>
-    </form>
+    <div className="max-w-md mx-auto">
+      <form onSubmit={handleSubmit} className="bg-gray-800 p-10 mb-4">
+      <h1 className="text-2x1 font-bold text-white mb-3">Create Task</h1>
+        <input
+          placeholder="Add task"
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+          className="bg-slate-300 p-3 w-full mb-2"
+          autoFocus
+        />
+        <textarea
+          placeholder="Task description"
+          onChange={(e) => setDescription(e.target.value)}
+          className="bg-slate-300 p-3 w-full mb-2"
+          value={description}
+        />
+        <button className="bg-indigo-500 px-3 py-1 text-white">Save</button>
+      </form>
+    </div>
   );
 }
 
